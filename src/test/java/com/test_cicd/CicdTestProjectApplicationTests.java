b8509e87472c0f66293a0ea617dce081432649ad
Chikason <chika.agbo@smartflowtech.com>
2022-12-31 16:37:38 +0100

    cicd test app

---
+++ src/test/java/com/test_cicd/CicdTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_cicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdTestProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
